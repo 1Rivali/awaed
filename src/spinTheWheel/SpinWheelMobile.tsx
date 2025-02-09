17fb025944a01800de341615b7b820359f69feb4
jesus is king <fadi.b.alhabib@gmail.com>
2025-02-09 12:11:26 +0200

    flip responsive

--- src/spinTheWheel/SpinWheelMobile.tsx
+++ src/spinTheWheel/SpinWheelMobile.tsx
@@ -10,23 +10,14 @@ import {
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
-import spinnerAudio from "../assets/audio/spin-232536.mp3";
-import countIcon from "../assets/basics/count-icon.svg";
-import awaedWritten from "../assets/basics/logo-written.svg";
-import arzLogo from "../assets/basics/powered-by-arz.svg";
-import statisticsButton from "../assets/basics/statistics-button.svg";
-import testIcon from "../assets/basics/tests-button.svg";
 import themeIcon from "../assets/basics/theme-button.svg";
-import bgGreen from "../assets/bgs/mobile-bg-green.svg";
-import gameOverBg from "../assets/bgs/game-over-bg.svg";
-import gameOverBackTmw from "../assets/game-over/back-tmw.svg";
-import gameOverComplete from "../assets/game-over/complete.svg";
-import gameOverSpinsOvr from "../assets/game-over/spins-ovr.svg";
-import gameOverThanks from "../assets/game-over/thanks.svg";
+import bgGreen from "../assets/bgs/bg-green.svg";
+import awaedWritten from "../assets/basics/logo-written.svg";
+import testIcon from "../assets/basics/tests-button.svg";
+import arzLogo from "../assets/basics/powered-by-arz.svg";
 import appleLogo from "../assets/spin-prizes/apple.svg";
 import aramcoLogo from "../assets/spin-prizes/aramco.svg";
 import googleLogo from "../assets/spin-prizes/google.svg";
-import loseImage from "../assets/spin-prizes/lost-the-turn.svg";
 import lucidLogo from "../assets/spin-prizes/lucid.svg";
 import nvidiaLogo from "../assets/spin-prizes/nvidia.svg";
 import sabicLogo from "../assets/spin-prizes/sabic.svg";
@@ -34,11 +25,20 @@ import snapLogo from "../assets/spin-prizes/snap.svg";
 import stcLogo from "../assets/spin-prizes/stc.svg";
 import spinnerBlack from "../assets/spinner-variants/spinner-black.png";
 import spinnerWhite from "../assets/spinner-variants/spinner-white.png";
-import LoseModal from "../components/modals/LoseModal";
-import StatsModal from "../components/modals/StatsModal";
 import TestsModal from "../components/modals/TestsModal";
 import ThemeModal from "../components/modals/ThemeModal";
 import WinModal from "../components/modals/WinModal";
+import loseImage from "../assets/spin-prizes/lost-the-turn.svg";
+import countIcon from "../assets/basics/count-icon.svg";
+import LoseModal from "../components/modals/LoseModal";
+import spinnerAudio from "../assets/audio/spin-232536.mp3";
+import gameOverBg from "../assets/bgs/game-over-bg.svg";
+import gameOverComplete from "../assets/game-over/complete.svg";
+import gameOverSpinsOvr from "../assets/game-over/spins-ovr.svg";
+import gameOverBackTmw from "../assets/game-over/back-tmw.svg";
+import gameOverThanks from "../assets/game-over/thanks.svg";
+import statisticsButton from "../assets/basics/statistics-button.svg";
+import StatsModal from "../components/modals/StatsModal";
 
 const SEGMENTS = [
   { image: appleLogo, currentPrice: "900", stockName: "AAPL", maxWinners: 2 },
@@ -91,7 +91,7 @@ const SpinWheel = () => {
   const [spinner, setSpinner] = useState<string>(spinnerBlack);
   const [wheel, setWheel] = useState<string>("black");
   const [spinAudio] = useState(new Audio(spinnerAudio));
-
+  const [toolBarVisible, setToolBarVisible] = useState(false);
   const [currentSpinIndex, setCurrentSpinIndex] = useState(0);
   const [pool, setPool] = useState<number[]>([]);
 
@@ -225,44 +225,81 @@ const SpinWheel = () => {
     onOpen: onStatsModalOpen,
     onClose: onStatsModalClose,
   } = useDisclosure();
+  const [showGameOver, setShowGameOver] = useState(false);
 
-  if (currentSpinIndex === 698) {
+  useEffect(() => {
+    if (currentSpinIndex === 698) {
+      const timer = setTimeout(() => {
+        setShowGameOver(true);
+      }, 5000); // 5 seconds delay
+
+      return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    }
+  }, [currentSpinIndex]);
+  if (currentSpinIndex === 698 && showGameOver) {
     return (
-      <Box
-        w={"100vw"}
-        height={"100vh"}
-        bg={`url(${gameOverBg})`}
-        bgSize={"cover"}
-        position={"relative"}
+      <motion.div
+        initial={{
+          opacity: 0.5,
+          y: 0,
+          background: `url(${bg})`,
+          backgroundSize: "cover",
+        }} // Initial state (hidden)
+        animate={{
+          opacity: 1,
+          y: 0,
+          background: `url(${gameOverBg})`,
+          backgroundSize: "cover",
+        }} // Animate to visible
+        transition={{ duration: 1, ease: "easeIn" }}
       >
-        <Center>
-          <VStack height={"90vw"} justifyContent={"space-around"}>
-            <Image mt={"8vw"} src={gameOverComplete} height={"25vw"} />
-            <Image src={gameOverSpinsOvr} height={"16vw"}></Image>
-            <Heading color={"white"} fontSize={"8vw"}>
-              Total Spins:{" "}
-              <Text as={"span"} color={"#FFB800"}>
-                698
-              </Text>
-            </Heading>
-            <Image src={gameOverBackTmw} height={"8vw"}></Image>
-            <Image src={gameOverThanks} height={"8vw"}></Image>
-          </VStack>
-        </Center>
         <Box
-          width={"100vw"}
-          position={"absolute"}
-          bottom={"0"}
-          left={"0"}
-          py={"3vw"}
-          pl={"5vw"}
-          pr={"5vw"}
-          zIndex={50}
+          w={"100vw"}
+          height={"100vh"}
+          bgSize={"cover"}
+          position={"relative"}
         >
-          <Image width={"30vw"} mb={"1vw"} src={awaedWritten} />
-          <Image width={"50vw"} src={arzLogo} />
+          <Center>
+            <VStack height={"90vw"} justifyContent={"space-around"}>
+              <Image mt={"8vw"} src={gameOverComplete} height={"25vw"} />
+              <Image src={gameOverSpinsOvr} height={"16vw"}></Image>
+              <Heading color={"white"} fontSize={"8vw"}>
+                Total Spins:{" "}
+                <Text as={"span"} color={"#FFB800"}>
+                  698
+                </Text>
+              </Heading>
+              <Image src={gameOverBackTmw} height={"8vw"}></Image>
+              <Image src={gameOverThanks} height={"8vw"}></Image>
+              <Text
+                as={"span"}
+                zIndex={"200"}
+                color={"red"}
+                cursor={"pointer"}
+                onClick={() => {
+                  localStorage.clear();
+                  window.location.reload();
+                }}
+              >
+                Reset
+              </Text>
+            </VStack>
+          </Center>
+          <Box
+            width={"100vw"}
+            position={"absolute"}
+            bottom={"0"}
+            left={"0"}
+            py={"3vw"}
+            pl={"5vw"}
+            pr={"5vw"}
+            zIndex={50}
+          >
+            <Image width={"30vw"} mb={"1vw"} src={awaedWritten} />
+            <Image width={"50vw"} src={arzLogo} />
+          </Box>
         </Box>
-      </Box>
+      </motion.div>
     );
   }
 
@@ -439,10 +476,10 @@ const SpinWheel = () => {
         display={"flex"}
         zIndex={"200"}
         dir="rtl"
-        // onMouseEnter={() => setToolBarVisible(true)}
-        // onMouseLeave={() => setToolBarVisible(false)}
+        onMouseEnter={() => setToolBarVisible(true)}
+        onMouseLeave={() => setToolBarVisible(false)}
       >
-        {
+        {toolBarVisible && (
           <>
             {" "}
             <Image
@@ -477,7 +514,7 @@ const SpinWheel = () => {
               height={"20vw"}
             />
           </>
-        }
+        )}
       </Box>
 
       <Box
