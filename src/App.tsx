394ec7d5ea74381ff361392411151eea217f2403
jesus is king <fadi.b.alhabib@gmail.com>
2025-02-09 12:34:28 +0200

    over test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Box } from "@chakra-ui/react";
-import { useBreakpointValue } from "@chakra-ui/react";
+
 import SpinWheelMobile from "./spinTheWheel/SpinWheelMobile";
-import SpinWheel from "./spinTheWheel/SpinWheel";
 
 const App = () => {
-  const isMobile = useBreakpointValue({ base: true, md: false });
   return (
     <Box>
       {" "}
