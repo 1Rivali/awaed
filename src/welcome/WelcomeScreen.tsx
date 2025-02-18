53b49c215d602a5f4ccd7a57319ee0013830340c
jesus is king <fadi.b.alhabib@gmail.com>
2025-02-18 17:38:26 +0200

    welcome screen

--- src/welcome/WelcomeScreen.tsx
+++ src/welcome/WelcomeScreen.tsx
@@ -70,12 +70,6 @@ const WelcomeScreen = () => {
             as={motion.h1}
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{
-              type: "spring",
-              stiffness: 100,
-              damping: 20,
-              delay: 0.3,
-            }}
           >
             X
           </Heading>
